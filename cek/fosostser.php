5bf7353dceff002e1c9813ef9b55eca19ab2dbd8
kontol-svg <4marketm5@gmail.com>
2026-04-14 11:13:13 +0700

    Update and rename foostser.php to fosostser.php

--- cek/fosostser.php
+++ cek/fosostser.php
@@ -2,64 +2,37 @@
 /**
  * The template for displaying the footer.
  *
- * @package GeneratePress
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
 ?>
-
-	</div>
-</div>
-
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
 <?php
-/**
- * generate_before_footer hook.
- *
- * @since 0.1
- */
-do_action( 'generate_before_footer' );
+	astra_content_after();
+
+	astra_footer_before();
+
+	astra_footer();
+
+	astra_footer_after();
 ?>
-
-<div <?php generate_do_attr( 'footer' ); ?>>
-	<?php
-	/**
-	 * generate_before_footer_content hook.
-	 *
-	 * @since 0.1
-	 */
-	do_action( 'generate_before_footer_content' );
-
-	/**
-	 * generate_footer hook.
-	 *
-	 * @since 1.3.42
-	 *
-	 * @hooked generate_construct_footer_widgets - 5
-	 * @hooked generate_construct_footer - 10
-	 */
-	do_action( 'generate_footer' );
-
-	/**
-	 * generate_after_footer_content hook.
-	 *
-	 * @since 0.1
-	 */
-	do_action( 'generate_after_footer_content' );
-	?>
-</div>
-
+	</div><!-- #page -->
 <?php
-/**
- * generate_after_footer hook.
- *
- * @since 2.1
- */
-do_action( 'generate_after_footer' );
-
-wp_footer();
+	astra_body_bottom();
+	wp_footer();
 ?>
 <div style="display:none"><ul><li><a href="https://boucherieus.org/">https://boucherieus.org/</a></li><li><a href="https://boucherieus.org/boucherie-west-village-menus/">https://boucherieus.org/boucherie-west-village-menus/</a></li><li><a href="https://boucherieus.org/location/boucherie-west-village/">https://boucherieus.org/location/boucherie-west-village/</a></li><li><a href="https://boucherieus.org/location/la-grande-boucherie-dc/">https://boucherieus.org/location/la-grande-boucherie-dc/</a></li><li><a href="https://boucherieus.org/location/chicago/">https://boucherieus.org/location/chicago/</a></li><li><a href="https://boucherieus.org/location/la-grande-boucherie-miami/">https://boucherieus.org/location/la-grande-boucherie-miami/</a></li><li><a href="https://boucherieus.org/events/">https://boucherieus.org/events/</a></li><li><a href="https://boucherieus.org/la-grande-ny-reservations/">https://boucherieus.org/la-grande-ny-reservations/</a></li><li><a href="https://boucherieus.org/la-grande-mia-reservations/">https://boucherieus.org/la-grande-mia-reservations/</a></li><li><a href="https://bubbagump.org/">https://bubbagump.org/</a></li><li><a href="https://bubbagump.org/location/bubba-gump-orlando-fl/">https://bubbagump.org/location/bubba-gump-orlando-fl/</a></li><li><a href="https://bubbagump.org/menu/food-bgkh/">https://bubbagump.org/menu/food-bgkh/</a></li><li><a href="https://bubbagump.org/view-all-locations/">https://bubbagump.org/view-all-locations/</a></li><li><a href="https://bubbagump.org/location/bubba-gump-gatlinburg-tn/">https://bubbagump.org/location/bubba-gump-gatlinburg-tn/</a></li><li><a href="https://indofood.org/product/sarimi">https://indofood.org/product/sarimi</a></li><li><a href="https://indofood.org/product/milkuat/product">https://indofood.org/product/milkuat/product</a></li><li><a href="https://indofood.org/product/tiga-sapi">https://indofood.org/product/tiga-sapi</a></li><li><a href="https://indofood.org/career/">hhttps://indofood.org/career/</a></li><li><a href="https://indofood.org/company/board-of-commissioners/hans-kartikahadi.html">https://indofood.org/company/board-of-commissioners/hans-kartikahadi.html</a></li></ul></div>
-</body>
+
+	</body>
 </html>
